e0f3faca5433985bffc52fae5bf9c4c8c84e6959
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2018-08-29 14:47:15 +0200

    Make app offline-first

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// Register service worker to cache responses to request for assets for offline use
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'));
 registerServiceWorker();
